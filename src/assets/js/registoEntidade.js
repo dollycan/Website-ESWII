7b98f39ab067316c11856eb2251744a766c365c3
dollycan <agouveiacardoso@hotmail.com>
2023-04-16 23:50:10 +0100

    Co-authored-by: Tiago2840 <Tiago2840@users.noreply.github.com>

---
+++ src/assets/js/registoEntidade.js
@@ -0,0 +1,9 @@
+const submit = document.querySelector('#form-outter-container button')
+
+function redirectOnSubmit() {
+    //consumidora => produtoConsumo
+    //produtora => registoProduto
+    window.location.href = '../../../registoProduto.html'
+    
+    window.location.href = '../../../produtoConsumo.html'
+}
